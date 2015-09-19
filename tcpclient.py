f5e5a4c1353a3b3f361ac4ede67cd735b1c3b1a8
foxever <foxever@Mac-3c970e8d7e71.lan>
2015-09-19 22:10:30 +0800

    basic implementation,demo

---
+++ tcpclient.py
@@ -0,0 +1,10 @@
+__author__ = 'foxever'
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 9999))
+print(s.recv(1024).decode('utf-8'))
+for data in ['Michael', 'Tracy', 'Sarah']:
+    s.send(data.encode('utf-8'))
+    print(s.recv(1024).decode('utf-8'))
+s.send(b'exit')
+s.close()
---
+++ tcpserver.py
@@ -0,0 +1,27 @@
+__author__ = 'foxever'
+import socket
+import threading
+import time
+
+def tcplink(sock, addr):
+    print('Accept new connection from %s:%s...' % addr)
+    sock.send(b'Welcome!')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        sock.send(('Hello,%s!' % data.decode('utf-8')).encode('utf-8'))
+    sock.close()
+    print('Connection from %s:%s close.' % addr)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#监听端口
+s.bind(('127.0.0.1', 9999))
+s.listen(5)
+print('Waiting for connection...')
+while True:
+    #接受一个新的连接:
+    sock, addr = s.accept()
+    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.start()
